63e491c0becbc6d27dd70faf0c88cf86d9082a2a
wf34 <wf34@users.noreply.github.com>
2025-07-19 20:58:51 +0300

    develops GtVisualObs and callback system

--- src/lib.rs
+++ src/lib.rs
@@ -8,12 +8,42 @@ use crate::oracle::GtObservation;
 #[pyclass]
 struct Runtime {
     fake_state: String,
+    pose_callback: Option<PyObject>,
+    counter: i32,
 }
 
 #[pymethods]
 impl Runtime {
-    fn hello_world(&self) {
+    fn hello_world(&mut self) {
         println!("The measurement is: {}", self.fake_state);
+
+        if 0 == self.counter % 10 {
+            let _ = self.trigger_pose_callback();
+        }
+
+        self.counter += 1;
+    }
+
+    fn set_pose_callback(&mut self, callback: PyObject) {
+        self.pose_callback = Some(callback);
+    }
+
+    fn trigger_pose_callback(&self) -> PyResult<()> {
+        if let Some(this_callback) = &self.pose_callback {
+             Python::with_gil(|py| {
+                 // let args = PyTuple::new(py, &[value]);
+                 // call1(py, args)
+                 match this_callback.call0(py) {
+                     Ok(_) => Ok(()),
+                     Err(e) => {
+                         eprintln!("Pose callback error: {}", e);
+                         Err(e)
+                     }
+                 }
+             })
+        } else {
+            Ok(())
+        }
     }
 
     fn add_observation(&self, observation: &GtObservation) {
@@ -23,7 +53,7 @@ impl Runtime {
 
     #[new]
     fn new(a: String) -> Self {
-        Runtime{fake_state: a}
+        Runtime{fake_state: a, pose_callback: None, counter: 0}
     }
 }
 
--- src/oracle.rs
+++ src/oracle.rs
@@ -1,5 +1,8 @@
 use pyo3::prelude::*;
 
+const INVALID_FLOAT_ENTRY: f32 = f32::MIN;
+const INVALID_ID_ENTRY: u32 = u32::MAX;
+
 #[pyclass]
 pub struct GtObservation {
     pub ts : f32,
@@ -10,9 +13,29 @@ pub struct GtObservation {
 #[pymethods]
 impl GtObservation {
     #[new]
-    fn new(time: f32, id_: u32, xy_tup: (f32, f32)) -> Self {
-        GtObservation{ts : time,
-                      id : id_,
-                      xy : xy_tup,}
+    #[pyo3(signature = (time = None, id_ = None, xy_tup = None))]
+    fn new(time: Option<f32>, id_: Option<u32>, xy_tup: Option<(f32, f32)>) -> Self {
+        Self{ts : time.unwrap_or(INVALID_FLOAT_ENTRY),
+             id : id_.unwrap_or(INVALID_ID_ENTRY),
+             xy : xy_tup.unwrap_or((INVALID_FLOAT_ENTRY, INVALID_FLOAT_ENTRY))}
+    }
+
+    fn is_valid(&self) -> bool {
+        INVALID_FLOAT_ENTRY != self.ts && 
+        INVALID_ID_ENTRY != self.id &&
+        INVALID_FLOAT_ENTRY != self.xy.0 &&
+        INVALID_FLOAT_ENTRY != self.xy.1
+    }
+
+    fn __getstate__(&self) -> PyResult<(f32, u32, f32, f32)> {
+        Ok((self.ts, self.id, self.xy.0, self.xy.1))
+    }
+
+    fn __setstate__(&mut self, state: (f32, u32, f32, f32)) -> PyResult<()> {
+        self.ts = state.0;
+        self.id = state.1;
+        self.xy.0 = state.2;
+        self.xy.1 = state.3;
+        Ok(())
     }
 }
