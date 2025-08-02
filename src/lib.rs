204f7e00b988caea464ab174b5b1f59aed68033b
wf34 <wf34@users.noreply.github.com>
2025-08-02 16:58:36 +0300

    filter to publish random pose at each `prop`

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
+use rand::prelude::*;
 use pyo3::prelude::*;
+
+use rand_distr::Normal;
 
 use pyo3::types::PyList;
 use pyo3::types::PyTuple;
@@ -10,10 +13,15 @@ pub mod front;
 use crate::oracle::GtObservation;
 use crate::measurement::InertialMeasurement;
 
+const SEED: u8 = 34;
+const SEED_N: usize = 32;
+
 #[pyclass]
 struct Runtime {
+    rng: StdRng,
     label: String,
     pose_callback: Option<PyObject>,
+    ptime: Option<f32>,
 }
 
 #[pymethods]
@@ -22,39 +30,71 @@ impl Runtime {
         self.pose_callback = Some(callback);
     }
 
-    fn trigger_pose_callback(&self) -> PyResult<()> {
-        if let Some(this_callback) = &self.pose_callback {
-             Python::with_gil(|py| {
-                 let rust_array = vec![0.0, 1.0, 2.5, 3.7, 4.2, 0., 0., 0.];
-                 let ts_and_pose = PyList::new(py, rust_array)?;
-                 let args = PyTuple::new(py, ts_and_pose)?;
-                 match this_callback.call1(py, args) {
-                     Ok(_) => Ok(()),
-                     Err(e) => {
-                         eprintln!("Pose callback error: {}", e);
-                         Err(e)
-                     }
-                 }
-             })
-        } else {
-            Ok(())
+    fn trigger_pose_callback(&mut self) -> PyResult<()> {
+        match(&self.pose_callback, &self.ptime) {
+            (Some(this_callback), Some(propagate_time)) => {
+                Python::with_gil(|py| {
+                    let n_a = Normal::new(0.0, 0.01).unwrap();
+                    let n_t = Normal::new(0.0, 0.1).unwrap();
+
+                    let mut arr = vec![
+                        *propagate_time
+                      , 1.0
+                      , n_a.sample(&mut self.rng)
+                      , n_a.sample(&mut self.rng)
+                      , n_a.sample(&mut self.rng)
+                      , n_t.sample(&mut self.rng)
+                      , n_t.sample(&mut self.rng)
+                      , n_t.sample(&mut self.rng)];
+
+                    let norm: f32 = (&arr[1].powf(2.) +
+                                     &arr[2].powf(2.) +
+                                     &arr[3].powf(2.) +
+                                     &arr[4].powf(2.)).sqrt();
+                    arr[1] /= norm;
+                    arr[2] /= norm;
+                    arr[3] /= norm;
+                    arr[4] /= norm;
+
+                    let ts_and_pose = PyList::new(py, arr)?;
+                    let args = PyTuple::new(py, &[ts_and_pose])?;
+                    match this_callback.call1(py, args) {
+                        Ok(_) => Ok(()),
+                        Err(e) => {
+                            eprintln!("Pose callback error: {}", e);
+                            Err(e)
+                        }
+                    }
+                })
+            }
+            _ => {
+                Ok(())
+            }
         }
     }
 
-    fn add_inertial_measurement(&self, _observation: &InertialMeasurement) {
-        let x = _observation.ts;
-        println!("{0} got IMU {x}", self.label);
+    fn add_inertial_measurement(&mut self, _observation: &InertialMeasurement) {
+        let curr_ts = _observation.ts;
+        // println!("{0} got IMU {curr_ts}", self.label);
+
+        assert!(self.ptime.is_none() || self.ptime.unwrap() < curr_ts);
+        self.ptime = Some(curr_ts);
+
+        let _ = self.trigger_pose_callback();
     }
 
     fn add_observation(&self, observation: &GtObservation) {
         let x = observation.ts;
-        println!("{0} got obs {x}", self.label);
+        // println!("{0} got obs {x}", self.label);
     }
 
     #[new]
     fn new(label: String) -> Self {
         println!("creates kalmity runtime {label}");
-        Runtime{label: label, pose_callback: None}
+        Runtime{rng: StdRng::from_seed([SEED; SEED_N]),
+                label: label,
+                pose_callback: None,
+                ptime: None}
     }
 }
 
